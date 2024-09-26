a425a1cf1510c867a4eb93ce4a279998d4229b99
iswarya <bandaruiswarya26@gmail.com>
2024-09-26 12:04:22 +0530

    forloop program

---
+++ ForLoop.java
@@ -0,0 +1,11 @@
+
+class ForLoop{
+	public static void main(String [] args){
+		int fact=1;
+		for(int num=6; num>1; num--){
+			fact=fact*num;
+		    
+		}
+		System.out.println("factorial of number is:" +fact);
+	}
+}
