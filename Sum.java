2102492d13bc475f3348233531d6303706ffcbe8
iswarya <bandaruiswarya26@gmail.com>
2024-09-26 15:33:16 +0530

    sum of numbers

---
+++ Sum.java
@@ -0,0 +1,16 @@
+
+class Sum{
+	public static void main(String [] args){
+		
+		int num=40;
+		int sum=0;
+		do{
+			sum=sum+num;
+		    num=num+2;
+		}
+		
+		while(num<=80);
+			System.out.print("the sum of even between 40 t0 80 is: "+sum);
+		
+	}
+}
